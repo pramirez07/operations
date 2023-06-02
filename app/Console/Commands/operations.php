b948e57f1c0ab1ffc5b2bb97514c98b3cdfef7d9
Paul Ramirez <paulramirez07@gmail.com>
2023-06-02 14:25:25 +0200

    Command

---
+++ app/Console/Commands/operations.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Http\Controllers\OperationController;
+use Illuminate\Console\Command;
+
+class operations extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'operations {operatorA} {operatorB} {operation}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Performs desired operation with its operators';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        switch($this->argument('operation')){
+            case "add":
+                $result = OperationController::add($this->argument('operatorA'),$this->argument('operatorB'));
+                $this->info("Result: {$result}");
+                break;
+            case "subtract":
+                $result = OperationController::subtract($this->argument('operatorA'),$this->argument('operatorB'));
+                $this->info("Result: {$result}");
+                break;
+            case "multiply":
+                $result = OperationController::multiply($this->argument('operatorA'),$this->argument('operatorB'));
+                $this->info("Result: {$result}");
+                break;
+            case "divide":
+                $result = OperationController::divide($this->argument('operatorA'),$this->argument('operatorB'));
+                $this->info("Result: {$result}");
+                break;
+            default:
+                $this->error("Operation type not supported.");
+        }
+        
+    }
+}
